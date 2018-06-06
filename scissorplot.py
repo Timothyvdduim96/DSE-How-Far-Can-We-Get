f321172ae49813936448939b7c297faacc744461
tittymaster96 <36629004+tittymaster96@users.noreply.github.com>
2018-06-06 12:40:53 +0200

    Pushi

--- scissorplot.py
+++ scissorplot.py
@@ -1,14 +1,18 @@
 from math import *
 import matplotlib.pyplot as pyplot
 
-#---------------------------------STABILITY----------------------------------------
+#---------------------------------PARAMETERS---------------------------------------
+
 S.M. = 0.05
+
+#---------------------------------STABILITY----------------------------------------
+
 m = (C_L_alpha_h/C_L_alpha_Ah*(1-downwash)*l_h/c_bar*(V_h/V)**2)**-1
-q = (x_bar_ac - S.M.)/(C_L_alpha_h/C_L_alpha_Ah*(1-downwash)*l_h/c_bar*(V_h/V)**2)
+q = -(x_bar_ac - S.M.)/(C_L_alpha_h/C_L_alpha_Ah*(1-downwash)*l_h/c_bar*(V_h/V)**2)
 
 Sh_S_stab = m*x_bar_cg + q
 
-#---------------------------------CONTROLLABILITY----------------------------------------
+#---------------------------------CONTROLLABILITY--------------------------------------
 
 n = (C_L_h/C_L_Ah*l_h/c_bar*(V_h/V)**2)**-1
 p = (C_m_ac/C_L_Ah-x_bar_ac)/(C_L_h/C_L_Ah*l_h/c_bar*(V_h/V)**2)
