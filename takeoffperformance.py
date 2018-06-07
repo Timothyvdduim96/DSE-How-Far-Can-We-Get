a231fa2f7d664fa3ca99984a04341d1099c7be8e
HELU230 <39803580+HELU230@users.noreply.github.com>
2018-06-07 17:57:22 +0200

    changes

--- takeoffperformance.py
+++ takeoffperformance.py
@@ -20,7 +20,7 @@ print "-------------Take-off performance-----------------"
 
 #------------------constants-------------
 rho_0 = 1.225
-MTOW = 72564.*g #Maximum take-off weight in kg
+MTOW = 68731.*g #Maximum take-off weight in kg
 #MTOW = 60000.
 #A = 15.91**2/30
 S = 128. #wing surface area in m^2
@@ -32,21 +32,21 @@ A = 14.
 e = 0.85
 gamma_climb = radians(3) #degrees
 hscr = 15.24 #screen height in m
-C_L_maxto = 2.01 #CL max takeoff
+C_L_maxto = (2.01)/(1.1**2) #CL max takeoff
 C_Lcr = 0.87 #CL max cruise
 C_Lland = 2.47  #CL max landing
 C_L_cr = 0.6167
-C_D_0 = 0.0165 #CDO
+C_D_0 = 0.07 #CDO
 thrust = 220
 #------------------------Intermediate calculations------------------
 V_min = sqrt((MTOW/S)*(2/rho_0)*(1/C_L_maxto)) #Minimum speed in m/s, use max CL during take-off
 #V_min = ((MTOW/S)*(2/rho_0)*(1/1.3))**0.5
-V_LOF = 1.05*V_min #Lift of speed in m/s
+V_LOF = 1.1*V_min #Lift of speed in m/s
 V = V_LOF/sqrt(2) #average speed
-CD = C_D_0 + (C_L_cr**2/(pi*A*e)) #use max clean?, Which value for CL, because drag polar only accurate up to a certain CL
+CD = C_D_0 + (C_L_maxto**2/(pi*A*e)) #use max clean?, Which value for CL, because drag polar only accurate up to a certain CL
 #CD = 0.08 + (0.6**2/(pi*A*e))
 D = CD * 0.5 * rho_0 * V**2 * S
-L = C_L_cr * 0.5 * rho_0 * V**2 * S
+L = C_L_maxto * 0.5 * rho_0 * V**2 * S
 #L = 0.6 * 0.5 * rho_0 * V**2 * S
 
 Dg = 0.02*(MTOW-L) #GROUND drag with a friction coefficient of 0.02
@@ -88,7 +88,7 @@ print "The total take-off distance is:", x_total, "m"
     
 
 print "-------------Landing performance"
-MLW = (72564.-6385)*g
+MLW = (68731.-6385)*g
 V_min_land = sqrt((MLW/S)*(2/rho_0)*(1/C_Lland))
 V_ap = 1.3*V_min_land
 gamma_approach = radians(3)
