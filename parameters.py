1e64976fe1767338768f89987b82fd4cbdd58dd6
Emad Adib <emad@wlan-145-94-153-240.wlan.tudelft.nl>
2018-06-08 13:43:56 +0200

    changes

--- parameters.py
+++ parameters.py
@@ -3,9 +3,6 @@ import numpy as np
 
 #------------------------------------------------CONVERSION--------------------------------------------------
 
-rad_to_deg = 180./pi
-deg_to_rad = pi/180.
-
 lbf_to_N = 4.4482216
 hr_to_s = 3600.
 lbm_to_kg = 0.45359237
@@ -80,12 +77,6 @@ def ISA(h):
 
     return T,p,rho
 
-# def ISA_rho(rho):
-#     T0 = T0lst[i]
-#     a  = lapselst[i]
-#     if rho > ISA(11000)[2]:
-#         h = (rho/1.225**((-g/(a*R)-1)**-1)-1)*T0/a
-#     return h
 
 def ISA_rho(rho):
     T0 = T0lst[0]
@@ -94,7 +85,6 @@ def ISA_rho(rho):
         h = ((rho/1.225)**((-g/(a*R)-1)**-1)-1)*T0/a
     return h
 
-print ISA_rho(0.8)
 #--------------speed------------------
 
 def a(h): #speed of sound
@@ -136,8 +126,4 @@ def cruise_thrust(h,C_D_cr,S):
     T_cr = 0.5*ISA(h)[2]*cruise_speed(h)**2*C_D_cr*S
 
     return T_cr
-    
-    
-    
-
 
