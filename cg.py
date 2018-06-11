bf88cc8db70d6dd87c3098b5e07670f27a795ccd
BartJacobson <39824152+BartJacobson@users.noreply.github.com>
2018-06-11 14:57:33 +0200

    fixed equipment added

--- cg.py
+++ cg.py
@@ -36,7 +36,7 @@ for i in range(2):
     
     def fus():
         x_cg = 0.435*l_fus
-        w = value("W_fuselage")/kglbs
+        w = value("W_fuselage")
         mom = x_cg*w
 
         return x_cg,w,mom
@@ -45,54 +45,61 @@ for i in range(2):
         span_pos = 0.35*b/2
         chord = c_r - (c_r-c_t)/((b-d_ext_fus/2)/2)*span_pos
         x_cg = chord*xstart+(xend-xstart)*0.7*chord + l_cabin*0.5 + l_cockpit - 0.25*MAC + cos(np.degrees(lambdac_0))*(0.35*(b/2-d_ext_fus/2) - (c_r - MAC)/((c_r-c_t)/((b-d_ext_fus/2)/2)))
-        w = value("W_wing")/kglbs #ADD
+        w = value("W_wing") #ADD
         mom = x_cg*w
 
         return x_cg,w,mom
 
     def hortail():
         x_cg = l_fus - (cos(np.radians(sweep_h))*b_h/2 + ct_h) + cos(np.radians(sweep_h))*0.38*b_h/2 + 0.42*MAC_h
-        w = value("W_empennage")/kglbs/4 #CHANGE
+        w = value("W_empennage")/4 #CHANGE
         mom = x_cg*w
 
         return x_cg,w,mom
 
     def verttail():
         x_cg = l_fus - 0.58*MAC_v
-        w = value("W_empennage")/kglbs/4*3 #CHANGE
+        w = value("W_empennage")/4*3 #CHANGE
         mom = x_cg*w
 
         return x_cg,w,mom
 
     def engines():
         x_cg = x_lemac + cos(np.degrees(lambdac_0))*(0.35*(b/2-d_ext_fus/2) - (c_r - MAC)/((c_r-c_t)/((b-d_ext_fus/2)/2))) - 0.3*ln
-        w = value("W_propulsion")/kglbs #ADD
+        w = value("W_propulsion") #ADD
         mom = x_cg*w
 
         return x_cg,w,mom
 
     def nacelle():
         x_cg = x_lemac + cos(np.degrees(lambdac_0))*(0.35*(b/2-d_ext_fus/2) - (c_r - MAC)/((c_r-c_t)/((b-d_ext_fus/2)/2))) - ln*0.4
-        w = value("W_nacelle")/kglbs #ADD
+        w = value("W_nacelle") #ADD
         mom = x_cg*w
 
         return x_cg,w,mom
 
     def mainlandinggear():
         x_cg = x_oew + 0.804 #CHECK
-        w = value("W_landinggear")/kglbs/10.*9. #CHANGE
+        w = value("W_landinggear")/10.*9. #CHANGE
         mom = x_cg*w
 
         return x_cg,w,mom
 
     def noselandinggear():
-        w = value("W_landinggear")/kglbs/10. #CHANGE
+        w = value("W_landinggear")/10. #CHANGE
         x_cg = x_oew - (mainlandinggear()[0] - x_oew)*mainlandinggear()[1]/w
         mom = x_cg*w
 
         return x_cg,w,mom
+        
+    def fixedequipment ():
+        w = value("W_equipment")
+        x_cg = 846./866.*0.435*l_fus
+        mom = x_cg*w
+        
+        return x_cg,w,mom
 
-    x_oew_up = (fus()[2] + wing()[2] + hortail()[2] + verttail()[2] + engines()[2] + nacelle()[2] + mainlandinggear()[2] + noselandinggear()[2])/(fus()[1] + wing()[1] + hortail()[1] + verttail()[1] + engines()[1] + nacelle()[1] + mainlandinggear()[1] + noselandinggear()[1])
+    x_oew_up = (fus()[2] + wing()[2] + hortail()[2] + verttail()[2] + engines()[2] + nacelle()[2] + mainlandinggear()[2] + noselandinggear()[2] + fixedequipment ()[2])/(fus()[1] + wing()[1] + hortail()[1] + verttail()[1] + engines()[1] + nacelle()[1] + mainlandinggear()[1] + noselandinggear()[1] + fixedequipment ()[1])
     print x_oew_up
     if i == 0:
         x_oew = x_oew_up
